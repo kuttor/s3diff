cf185dc4541753200a339b453b654016d8a028de
Andrew Kuttor <andrew.kuttor@gmail.com>
2016-05-06 10:10:37 -0700

    Getting close, added better chop logic

--- s3diff.py
+++ s3diff.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-s3diff - utility for determining differences between buckets.
+s3diff - utility that performs an e-tag lis buckets.
 
 Usage:
   s3diff.py <left-bucket> <right-bucket>
@@ -12,6 +12,10 @@ Options:
 """
 from boto3 import client
 from docopt import docopt, DocoptExit
+from multiprocessing import Process
+from time import time, clock, sleep
+
+
 
 __author__ = "Andrew Kuttor"
 __credits__ = "Andrew Kuttor"
@@ -24,39 +28,50 @@ __status__ = "Development"
 def main():
     try:
         arguments = docopt(__doc__, version='s3diff 1.0')
-        left = keys(splitter(arguments['<left-bucket>']))
-        right = keys(splitter(arguments['<right-bucket>']))
-
-    # invalid argument handler
+        parallel(
+            left=keys(chop(arguments['<left-bucket>'])),
+            right=keys(chop(arguments['<right-bucket>']))
+        )
     except DocoptExit as exit:
         print exit.message
 
 
 # splits bucket and the first / to seperate prefix
-def splitter(bucket):
-    divorced = bucket.split('/', 1)
-    params = {'Bucket': divorced[0]}
-    if len(divorced) == 2:
-        params = {"Bucket": divorced[0], "Prefix": divorced[1]}
+def chop(path):
+    cleave = path.split('/', 1)
+    params = {'Bucket': cleave[0]}
+    if len(cleave) == 2:
+        if not cleave[1].endswith("/"):
+            cleave[1] = cleave[1] + "/"
+        params = {"Bucket": cleave[0], "Prefix": cleave[1]}
     return params
 
 
 # creates list of object etags recursively starting at bucket/prefix
 def keys(args):
+    print "\nGenerating list of all e-tags for:", args['Bucket']
     paginator = client('s3').get_paginator('list_objects')
     pages = paginator.paginate(**args)
-
-    print "\nGenerating list of all e-tags for:", args['Bucket']
     etag_list = []
-    # list all keys per iterated page
     for page in pages:
         for key in page['Contents']:
             etag_list.append(key['ETag'])
-
     return etag_list
 
+
+# executes two processes in parallel
+def parallel(left, right):
+    procs = []
+    procs.append(Process(target=left))
+    procs.append(Process(target=right))
+    map(lambda x: x.start(), procs)
+    map(lambda x: x.join(), procs)
+
+
+# --------------------------------------------------------------------------
 if __name__ == "__main__":
     main()
+
 
 # source: installation and usage manual
 # url: https://pythonhosted.org/joblib
